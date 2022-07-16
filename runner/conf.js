654101654838f87ed6efae6cadf57fd51c24dd52
Koustav Das <qacolours@gmail.com>
2022-07-17 00:43:23 +0530

    changed conf.js to remove the comments

--- runner/conf.js
+++ runner/conf.js
@@ -56,46 +56,4 @@ exports.config = {
         })
       });
   }
-  /*onPrepare: function () {
-    jasmine.getEnv().addReporter(
-      new SpecReporter({
-        suite: {
-          displayNumber: true     //display each suite number in hierarchial manner
-        },
-        spec: {
-          displayStacktrace: 'none',
-          displayPending: true,       //display each pending spec
-          displayDuration: true       //display each spec duration
-        },
-        summary: {
-          displaySuccessful: true,    //display summery of all successes after execution
-          displayFailed: true,        //display summery of all failures after execution
-          displayPending: true        //display summery of all pending specs after execution
-        }
-      })
-    );
-
-    var AllureReporter = require('jasmine-allure-reporter');
-    jasmine.getEnv().addReporter(new AllureReporter({
-      resultsDir: 'allure-results'
-    }));
-
-    jasmine.getEnv().beforeEach(function(done){
-      browser.takeScreenshot().then(function (png) {
-        allure.createAttachment('Screenshot', function () {
-          return new Buffer(png, 'base64')
-        }, 'image/png')();
-        done();
-      })
-    });
-
-    jasmine.getEnv().afterEach(function(done){
-      browser.takeScreenshot().then(function (png) {
-        allure.createAttachment('Screenshot', function () {
-          return new Buffer(png, 'base64')
-        }, 'image/png')();
-        done();
-      })
-    });
-  }*/
 };
